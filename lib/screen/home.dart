1de621b4fa1f6b39094b879823bfcf94123ee432
Tharathaep <tharathaep3@gmail.com>
2025-06-14 15:49:47 +0700

    Step 7: Button widget

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -56,17 +56,77 @@ class Home extends StatelessWidget {
     // );
 
     // Step 6: Image widget
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [Image.asset("assets/images/IMG_2995.png", width: 150,height: 150,),
-      const SizedBox(
-        height: 10,),
-        Image.network(
-          "https://instagram.fbkk22-4.fna.fbcdn.net/v/t51.2885-15/503123588_18087194929728225_8407901525892717639_n.webp?efg=eyJ2ZW5jb2RlX3RhZyI6IkNBUk9VU0VMX0lURU0uaW1hZ2VfdXJsZ2VuLjE0NDB4MTkyMC5zZHIuZjc1NzYxLmRlZmF1bHRfaW1hZ2UifQ&_nc_ht=instagram.fbkk22-4.fna.fbcdn.net&_nc_cat=109&_nc_oc=Q6cZ2QF5qxzNwtWHVdFNnrparCfBAXbhBdet__YcFhryq8cb1yUF8iJwkD5dGYuZgK87-N8&_nc_ohc=wIsLSCBHZzEQ7kNvwEDoq51&_nc_gid=ilhJpTFJxz38KIvVySQ2VA&edm=AP4sbd4BAAAA&ccb=7-5&ig_cache_key=MzY0NjA4Mjc3NjAzMzYyNTQwNw%3D%3D.3-ccb7-5&oh=00_AfM0K66sS5I-Foe0jA9_RurHru25hxufqFDMF2yvgbhjeA&oe=685312CE&_nc_sid=7a9f4b",
-          width: 150,
-          height: 150,
-        )],
+    // return Column(
+    //   mainAxisAlignment: MainAxisAlignment.center,
+    //   crossAxisAlignment: CrossAxisAlignment.center,
+    //   children: [Image.asset("assets/images/IMG_2995.png", width: 150,height: 150,),
+    //   const SizedBox(
+    //     height: 10,),
+    //     Image.network(
+    //       "https://instagram.fbkk22-4.fna.fbcdn.net/v/t51.2885-15/503123588_18087194929728225_8407901525892717639_n.webp?efg=eyJ2ZW5jb2RlX3RhZyI6IkNBUk9VU0VMX0lURU0uaW1hZ2VfdXJsZ2VuLjE0NDB4MTkyMC5zZHIuZjc1NzYxLmRlZmF1bHRfaW1hZ2UifQ&_nc_ht=instagram.fbkk22-4.fna.fbcdn.net&_nc_cat=109&_nc_oc=Q6cZ2QF5qxzNwtWHVdFNnrparCfBAXbhBdet__YcFhryq8cb1yUF8iJwkD5dGYuZgK87-N8&_nc_ohc=wIsLSCBHZzEQ7kNvwEDoq51&_nc_gid=ilhJpTFJxz38KIvVySQ2VA&edm=AP4sbd4BAAAA&ccb=7-5&ig_cache_key=MzY0NjA4Mjc3NjAzMzYyNTQwNw%3D%3D.3-ccb7-5&oh=00_AfM0K66sS5I-Foe0jA9_RurHru25hxufqFDMF2yvgbhjeA&oe=685312CE&_nc_sid=7a9f4b",
+    //       width: 150,
+    //       height: 150,
+    //     )],
+    // );
+
+    // Step 7: Button widget
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextButton(
+            style: TextButton.styleFrom(foregroundColor: Colors.red),
+            onPressed: () {
+              print("Button Pressed");
+            },
+            child: Text(
+              "Click Me!",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          const SizedBox(height: 10),
+          FilledButton(
+            style: FilledButton.styleFrom(
+              backgroundColor: Colors.blue,
+              foregroundColor: Colors.white,
+            ),
+            onPressed: () {
+              print("Filled Button Pressed");
+            },
+            child: Text(
+              "Click Me!",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.green,
+              side: BorderSide(color: Colors.green, width: 2),
+            ),
+            onPressed: () {
+              print("Outlined Button Pressed");
+            },
+            child: Text(
+              "Click Me!",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.purple,
+              foregroundColor: Colors.white,
+            ),
+            onPressed: () {
+              print("Elevated Button Pressed");
+            },
+            child: Text(
+              "Click Me!",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
