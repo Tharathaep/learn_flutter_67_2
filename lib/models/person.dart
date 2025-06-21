3daa099c3689c214e292bce0d06a8965562ef581
Tharathaep <tharathaep3@gmail.com>
2025-06-21 13:59:54 +0700

    Step 10+11

--- lib/models/person.dart
+++ lib/models/person.dart
@@ -1,17 +1,51 @@
+import 'package:flutter/material.dart';
+
 //Step 10:make a model for store data
+// class Person {
+//   String name;
+//   int age;
+//   String job;
+
+//   // Constructor
+//   Person({required this.name, required this.age , required this.job});
+// }
+
+//   List<Person> people = [
+//     Person(name: "สมชาย", age: 30, job: "Engineer"),
+//     Person(name: "สมหญิง", age: 28, job: "Doctor"),
+//     Person(name: "สมศรี", age: 25, job: "Teacher"),
+//     Person(name: "สมปอง", age: 35, job: "Artist"),
+//     Person(name: "สมจิตร", age: 40, job: "Chef"),
+//   ];
+//Step 11: use a num
+enum Job {
+  Engineer(title: "Engineer", image: "assets/engineer.png", color: Colors.blue),
+  Doctor(title: "Doctor", image: "assets/doctor.png", color: Colors.red),
+  Teacher(title: "Teacher", image: "assets/teacher.png", color: Colors.green),
+  Artist(title: "Artist", image: "assets/artist.png", color: Colors.purple),
+  Chef(title: "Chef", image: "assets/chef.png", color: Colors.orange);
+
+  final String title;
+  final String image;
+  final Color color;
+
+  const Job({required this.title, required this.image, required this.color});
+}
+
+
 class Person {
   String name;
   int age;
-  String job;
+  Job job;
 
   // Constructor
-  Person({required this.name, required this.age , required this.job});
+  Person({required this.name, required this.age, required this.job});
 }
 
-  List<Person> people = [
-    Person(name: "สมชาย", age: 30, job: "Engineer"),
-    Person(name: "สมหญิง", age: 28, job: "Doctor"),
-    Person(name: "สมศรี", age: 25, job: "Teacher"),
-    Person(name: "สมปอง", age: 35, job: "Artist"),
-    Person(name: "สมจิตร", age: 40, job: "Chef"),
-  ];
+List<Person> people = [
+    Person(name: "สมชาย", age: 30, job: Job.Engineer),
+    Person(name: "สมหญิง", age: 28, job: Job.Doctor),
+    Person(name: "สมศรี", age: 25, job: Job.Teacher),
+    Person(name: "สมปอง", age: 35, job: Job.Artist),
+    Person(name: "สมจิตร", age: 40, job: Job.Chef),
+];
--- lib/screen/item.dart
+++ lib/screen/item.dart
@@ -1,4 +1,11 @@
+// // step8: stateful widget
 import 'package:flutter/material.dart';
+
+// Step 10: make a model for store data
+import 'package:learn_flutter_67_2/models/person.dart';
+
+// Step 12: use google fonts
+// import 'package:google_fonts/google_fonts.dart';
 
 class Item extends StatefulWidget {
   const Item({super.key});
@@ -8,29 +15,129 @@ class Item extends StatefulWidget {
 }
 
 class _ItemState extends State<Item> {
-  List<String> data = ["สมชาย", "สมหญิง", "สมศรี", "สมปอง", "สมจิตร"];
+  int quantity = 10;
+
+  // Step 11: Define the people list using the Person model
+  List<Person> people = [
+    Person(name: "สมชาย", age: 30, job: Job.Engineer),
+    Person(name: "สมหญิง", age: 28, job: Job.Doctor),
+    Person(name: "สมศรี", age: 25, job: Job.Teacher),
+    Person(name: "สมปอง", age: 35, job: Job.Artist),
+    Person(name: "สมจิตร", age: 40, job: Job.Chef),
+  ];
+
+  // void addQuantity() {
+  //     setState(() {
+  //       quantity++;
+  //     });
+  //   }
+
+  // void subtractQuantity() {
+  //     setState(() {
+  //       if (quantity > 0) {
+  //         quantity--;
+  //       }
+  //     });
+  //   }
+
+  // step 9: Listview widget
+  //List data = ["Isak", "Wirtz", "Kerkaze", "Frimpong", "Mamardashvili"];
 
   @override
   Widget build(BuildContext context) {
+    // return Center(
+    //   child: Column(
+    //     mainAxisAlignment: MainAxisAlignment.center,
+    //      children: [
+    //       Text(
+    //         "Quantity: $quantity",
+    //         style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    //       ),
+    //       const SizedBox(height: 20),
+    //       Row(
+    //         mainAxisAlignment: MainAxisAlignment.center,
+    //         children: [
+    //           ElevatedButton(
+    //             onPressed: addQuantity,
+    //             child: const Text("Add"),
+    //           ),
+    //           const SizedBox(width: 20),
+    //           ElevatedButton(
+    //             onPressed: subtractQuantity,
+    //             child: const Text("Subtract"),
+    //           ),
+    //         ],
+    //       ),
+    //      ],
+    //   ),
+    // );
     return ListView.builder(
-      itemCount: data.length,
       itemBuilder: (context, index) {
         return Container(
           decoration: BoxDecoration(
+            // color: Colors.grey,
             borderRadius: BorderRadius.circular(10),
-            color: Colors.blueAccent,
+            // step 11: use a enum
+            color: people[index].job.color,
           ),
-          margin: const EdgeInsets.all(16),
-          padding: const EdgeInsets.all(20),
-          child: Text(
-            data[index],
-            style: const TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-            ),
+          margin: EdgeInsets.symmetric(vertical: 2, horizontal: 5),
+          padding: EdgeInsets.all(40),
+          // child: Text(
+          //   data[index],
+          //   style: TextStyle(
+          //     fontSize: 24,
+          //     color: Colors.white,
+          //     fontWeight: FontWeight.bold,
+          //   ),
+          // ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    people[index].name,
+                    style: const TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "${people[index].age} years old",
+                    style: const TextStyle(fontSize: 20, color: Colors.white70),
+                  ),
+                  Text(
+                    "Job: ${people[index].job}",
+                    style: const TextStyle(fontSize: 20, color: Colors.white70),
+                  ),
+                  Image.asset("assets/images/IMG_2995.png", 
+                  width: 50, 
+                  height: 50),
+                ],
+              ),
+              // Text(
+              //   "${people[index].name} - ${people[index].age} - ${people[index].job}",
+              //   style: const TextStyle(
+              //     fontSize: 24,
+              //     color: Colors.white,
+              //     fontWeight: FontWeight.bold,
+              //   ),
+              // ),
+              IconButton(
+                icon: const Icon(Icons.delete, color: Colors.red),
+                onPressed: () {
+                  setState(() {
+                    people.removeAt(index);
+                  });
+                },
+              ),
+            ],
           ),
         );
       },
+      itemCount: people.length,
     );
   }
 }
